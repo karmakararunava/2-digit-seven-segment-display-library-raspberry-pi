ab95f9470262093070da8458b514558011802132
Arunava Karmakar <arunavo111@gmail.com>
2017-07-11 02:54:54 +0530

    Update oneDigitCC.h

--- oneDigitCC.h
+++ oneDigitCC.h
@@ -3,9 +3,9 @@
 ****************************************************************
 *           Define A through G and DP as global variables      *
 *             in the source code containing main()             *
-*           		       Note:- DP not used                      *
+*                      Note:- DP not used                      *
 *                             eg.:-                            *
-*                 int A=0(wiringPi pin number)                 *
+*                 int A = 0(wiringPi pin number)               *
 *                                                              *
 *                                                              *
 *                  Pin       |      CC_Display                 *
